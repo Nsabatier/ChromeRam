c4538b1c7c799c941abb363accda4395302a39f5
Nicolas Sabatier <nicolassabatier@MacBook-Pro-de-Nicolas-Sabatier.local>
2011-08-30 17:53:17 +0200

    Initial Commit

---
+++ ChromeRAM/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ChromeRAM
+//
+//  Created by Nicolas Sabatier on 30/08/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
---
+++ ChromeRAMTests/ChromeRAMTests.h
@@ -0,0 +1,13 @@
+//
+//  ChromeRAMTests.h
+//  ChromeRAMTests
+//
+//  Created by Nicolas Sabatier on 30/08/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ChromeRAMTests : SenTestCase
+
+@end
